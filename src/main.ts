16732f045430cd9f1afa17c668cf778f13f8f38d
MisterRuby <ruby8700@naver.com>
2022-11-11 19:18:59 +0900

    update : API 경로의 api prefix 제거

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.setGlobalPrefix('/api');
 
   const port = process.env.PORT;
   await app.listen(port);
