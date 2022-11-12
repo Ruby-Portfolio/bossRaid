7025deeac5c73f0b2a4a23e2427ea59f6bf6a4f4
MisterRuby <ruby8700@naver.com>
2022-11-12 14:13:30 +0900

    feat : 요청시 Validation 검증 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { pipeConfig } from './config/pipeConfig';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  pipeConfig(app);
 
   const port = process.env.PORT;
   await app.listen(port);
